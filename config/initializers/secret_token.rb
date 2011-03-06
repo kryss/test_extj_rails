71a6066db7f27afa6338d866bcecf1f960d71208
Kryss <kryss.col@gmail.com>
2011-03-06 14:36:55 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestExtjRails::Application.config.secret_token = '21563bc6d1c93799cb3dc87fb25e76145e64595fa1e3bb9e544e1c011e36ff556370bf40067551ddeedd4d98cb78458a3daa5ff42e8f3917b00d057f1b495d5b'
